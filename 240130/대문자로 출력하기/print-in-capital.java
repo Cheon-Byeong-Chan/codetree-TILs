46f017090fbd431cfb874d7028432e1f3167b45e
Cheon-Byeong-Chan <76854159+Cheon-Byeong-Chan@users.noreply.github.com>
2024-01-30 18:52:44 +0900

    Title: 대문자로 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/대문자로 출력하기/print-in-capital.java
+++ 240130/대문자로 출력하기/print-in-capital.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
         char[] arr= sc.next().toCharArray();
+
         for(int i = 0; i<arr.length;i++) {
-            if((int)arr[i] > (int)'A') {
-                if((int)arr[i] > (int)'a') {
+             
+                if(arr[i] >= 'a' && arr[i] <='z') {
                     System.out.print((char)(arr[i] - 'a' + 'A'));
-                } else {
+                } else if(arr[i] >= 'A' && arr[i] <= 'Z') {
                     System.out.print((arr[i]));
                 }
-            }
+            
         }
     }
 }
